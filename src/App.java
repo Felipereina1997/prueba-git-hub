727cd57803a93aadfa53fd8963172fb60c38c7bd
Felipe <felipereina1997@hotmail.com>
2026-08-12 15:07:28 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*Para llevar un proyecto a Git que es local
+        git init
+        git add .
+        git comit -m "Aqui pongo el mensaje que desee"
+
+                Y LUEGO PARA GIT HUB    
+
+        ya debo tener cerado el reporsitorio 
+        
+        */
+        System.out.println("Hello, World estudante Java!!");
+    }
+
+
+}
