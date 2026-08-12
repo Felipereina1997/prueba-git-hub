c49d200a71dc5206d51e094025a01f5c3a40461f
Felipe <felipereina1997@hotmail.com>
2026-08-12 15:26:10 -0500

    Second features agregado

--- src/App.java
+++ src/App.java
@@ -1,18 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        /*Para llevar un proyecto a Git que es local
-        git init
-        git add .
-        git comit -m "Aqui pongo el mensaje que desee"
-
-                Y LUEGO PARA GIT HUB    
-
-        ya debo tener cerado el reporsitorio 
-        
-        */
+        /*
+         * Para llevar un proyecto a Git que es local
+         * git init
+         * git add .
+         * git comit -m "Aqui pongo el mensaje que desee"
+         * 
+         * Y LUEGO PARA GIT HUB
+         * 
+         * ya debo tener cerado el reporsitorio
+         * 
+         */
         System.out.println("Hello, World estudante Java!!");
+        System.out.println("Este es un cambio");
     }
-
 
 }
